4819ab94203c14abc03c1544672ef58dab652c46
NandaFardian <fardiannanda09@gmail.com>
2024-01-09 11:35:50 +0700

    update database

--- database/db_olshop_projek.sql
+++ database/db_olshop_projek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2023 at 05:15 AM
+-- Generation Time: Jan 09, 2024 at 05:34 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -32,6 +32,7 @@ CREATE TABLE `invoice` (
   `invoice` varchar(255) NOT NULL,
   `userid` int(11) NOT NULL,
   `mPembayaran` text NOT NULL,
+  `total` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -39,8 +40,9 @@ CREATE TABLE `invoice` (
 -- Dumping data for table `invoice`
 --
 
-INSERT INTO `invoice` (`id`, `invoice`, `userid`, `mPembayaran`, `status`) VALUES
-(1, 'PW20014564111', 2, 'tes', 'ashahs');
+INSERT INTO `invoice` (`id`, `invoice`, `userid`, `mPembayaran`, `total`, `status`) VALUES
+(2, '20240102021110', 4, 'COD', '500000000', 'Belum Upload Bukti Tranfer'),
+(3, '20240102021159', 4, 'COD', '504000000', 'Belum Upload Bukti Tranfer');
 
 -- --------------------------------------------------------
 
@@ -50,18 +52,20 @@ INSERT INTO `invoice` (`id`, `invoice`, `userid`, `mPembayaran`, `status`) VALUE
 
 CREATE TABLE `invoice_detail` (
   `id` int(11) NOT NULL,
-  `invoiceid` int(11) NOT NULL,
+  `invoice` varchar(255) NOT NULL,
   `produkid` int(11) NOT NULL,
   `qty` varchar(255) NOT NULL,
-  `total` varchar(255) NOT NULL
+  `harga` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `invoice_detail`
 --
 
-INSERT INTO `invoice_detail` (`id`, `invoiceid`, `produkid`, `qty`, `total`) VALUES
-(1, 1, 1, '20', '50000000');
+INSERT INTO `invoice_detail` (`id`, `invoice`, `produkid`, `qty`, `harga`) VALUES
+(2, '20240102021110', 7, '1', '500000000'),
+(3, '20240102021159', 5, '1', '4000000'),
+(4, '20240102021159', 7, '1', '500000000');
 
 -- --------------------------------------------------------
 
@@ -79,7 +83,10 @@ CREATE TABLE `kategori` (
 --
 
 INSERT INTO `kategori` (`id`, `nama`) VALUES
-(1, 'Sepatu');
+(1, 'Sepatu'),
+(4, 'Baju'),
+(5, 'Celana'),
+(7, 'Sempak');
 
 -- --------------------------------------------------------
 
@@ -92,15 +99,15 @@ CREATE TABLE `keranjang` (
   `produkid` int(11) NOT NULL,
   `userid` int(11) NOT NULL,
   `qty` varchar(255) NOT NULL,
-  `total` varchar(255) NOT NULL
+  `harga` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `keranjang`
 --
 
-INSERT INTO `keranjang` (`id`, `produkid`, `userid`, `qty`, `total`) VALUES
-(1, 1, 1, '500', '5000000');
+INSERT INTO `keranjang` (`id`, `produkid`, `userid`, `qty`, `harga`) VALUES
+(19, 5, 4, '1', '4000000');
 
 -- --------------------------------------------------------
 
@@ -123,9 +130,8 @@ CREATE TABLE `produk` (
 --
 
 INSERT INTO `produk` (`id`, `nama`, `kategoriid`, `harga`, `keterangan`, `tanggal`, `gambar`) VALUES
-(1, 'Sepatu Pacok', 1, '5000000', 'Sangat Cocok digunakan untuk dikalangan adek kecil', '2023-11-21 00:00:00', '21112023135042acer_acer_projector_bs-020.jpg'),
 (5, 'Sepatu Super', 1, '4000000', 'Sangat Cocok digunakan untuk dikalangan adek kecil', '2023-11-21 00:00:00', '21112023135007847a4af1-5dfa-4299-8f35-b859f4ce5214.jpg'),
-(7, 'Sepatu Pacok', 1, '5000000', 'Sangat Cocok digunakan untuk dikalangan adek kecil', '2023-11-21 12:20:30', '21112023135335acer_acer_projector_bs-020.jpg');
+(7, 'Proyektor', 1, '500000000', 'Sangat Cocok digunakan untuk dikalangan adek kecilll dan ibu ibu hamil', '2023-12-05 10:11:00', '21112023135335acer_acer_projector_bs-020.jpg');
 
 -- --------------------------------------------------------
 
@@ -149,9 +155,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nama`, `email`, `alamat`, `noHp`, `password`, `level`, `gambar`) VALUES
-(1, 'Bang Mus', 'bangmus@gmail.com', 'Blang Padang Belok Kiri Dikit', '085732050792', '827ccb0eea8a706c4c34a16891f84e7b', '1', '28112023181429pexels-pixabay-65894.jpg'),
-(4, 'Simus', 'simus@gmail.com', 'Lampaseh Ujung Timur Barat Ke Kanan', '082236611615', '827ccb0eea8a706c4c34a16891f84e7b', '2', 'no-image.png'),
-(5, 'Nanda', 'ssss@gmail.com', 'Rumah Kita', '082236611615', '7e4f882d2ea10f9f8a665f9d92e4087f', '2', 'no-image.png');
+(1, 'Bang Mus', 'bangmus@gmail.com', 'Blang Padang Belok Kiri Dikit', '085732050792', '827ccb0eea8a706c4c34a16891f84e7b', '1', '051220230848271343071.png'),
+(4, 'Simus', 'simus@gmail.com', 'Lampaseh Ujung Timur Barat Ke Kanan', '082236611615', 'e10adc3949ba59abbe56e057f20f883e', '2', '131220230251471336343.png'),
+(5, 'Nanda', 'ssss@gmail.com', 'Rumah Kita', '082236611615', '7e4f882d2ea10f9f8a665f9d92e4087f', '2', 'no-image.png'),
+(6, 'nanda', 'nanda12@gmail.com', 'situ', '085233665455', '6b53d66024790d088b49d5417b318aa0', '2', 'no-image.png'),
+(7, 'Joko', 'joko@gmail.com', 'Daerah Lembah Dibawah Permukaan dan didalam Kedalaman Laut', '085732050792', '827ccb0eea8a706c4c34a16891f84e7b', '2', 'no-image.png');
 
 --
 -- Indexes for dumped tables
@@ -201,37 +209,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `invoice`
 --
 ALTER TABLE `invoice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `invoice_detail`
 --
 ALTER TABLE `invoice_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `keranjang`
 --
 ALTER TABLE `keranjang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
